a052cace99e23d8af7755c51918c7966e45eb08a
Yoon321 <tkdwls734@gmail.com>
2024-08-06 13:42:07 +0900

    KO-1 [FEAT] 스프링 부트 프로젝트 셋팅
    - spring boot로 프로젝트 초기 설정
    - lombok, web, h2 관련 의존 관계 설정 (기본적인 의존관계만 설정)
    - gitignore 파일 수정

---
+++ src/test/java/com/example/kickoffbackend/KickOffBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kickoffbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickOffBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
