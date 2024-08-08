a81f4a2f63f89a91a9b1f143f828714176a3ee04
Yoon321 <tkdwls734@gmail.com>
2024-08-08 09:49:25 +0900

    KO-3 [FEAT] 시큐리티 설정과 Validation 의존성 추가

--- src/main/java/com/example/kickoffbackend/config/SecurityConfig.java
+++ src/main/java/com/example/kickoffbackend/config/SecurityConfig.java
@@ -13,6 +13,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((auth) -> auth.disable())
+                .formLogin((auth) -> auth.disable())
+                .httpBasic((auth) -> auth.disable())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/join").permitAll()
+                );
         return http.build();
     }
 
