4d2794430fa92210ba36a75427c41f95037fdc71
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-08-21 18:16:19 +0900

    KO-9 [FEAT] 팀이름 중복 체크 기능

--- src/main/java/com/example/kickoffbackend/team/repository/TeamRepository.java
+++ src/main/java/com/example/kickoffbackend/team/repository/TeamRepository.java
@@ -5,6 +5,9 @@ import com.example.kickoffbackend.team.repository.custom.TeamCustom;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamRepository extends JpaRepository<Team, Long>, TeamCustom {
 
+    Optional<Team> findByTeamName(String teamName);
 }
