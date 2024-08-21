4354cbb174bbf0affc8a23c520e0e8b341612f45
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-08-21 20:20:04 +0900

    KO-9 [FEAT] 팀 필드 추가

--- src/main/java/com/example/kickoffbackend/team/dto/response/TeamResponse.java
+++ src/main/java/com/example/kickoffbackend/team/dto/response/TeamResponse.java
@@ -1,5 +1,8 @@
 package com.example.kickoffbackend.team.dto.response;
 
+import com.example.kickoffbackend.team.domain.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,13 +19,21 @@ public class TeamResponse {
 
     private String teamName;
 
-    private String introduction;
+    private String teamIntroduction;
 
-    private String rule;
+    private String teamRule;
 
-    private MultipartFile teamFile; // 파일 담는 용도
-    private String originalFileName; // 원본 파일 이름
-    private String storedFileName; // 저장된 파일 이름
-    private int fileAttached; // 파일 이름 여부(첨부 1, 미첨부 0)
+    private int fileAttached;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String address;
+
+    private String teamLevel;
+
+    private String originalTeamImageName;
+
+    private String storedTeamImageName;
 
 }
