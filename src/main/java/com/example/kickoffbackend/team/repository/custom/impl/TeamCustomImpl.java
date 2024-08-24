b1dc9b209bc6316a02adec7470dfd347d6d8fae2
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-08-24 17:56:26 +0900

    KO-11 [FEAT] 팀 조회 API

--- src/main/java/com/example/kickoffbackend/team/repository/custom/impl/TeamCustomImpl.java
+++ src/main/java/com/example/kickoffbackend/team/repository/custom/impl/TeamCustomImpl.java
@@ -1,8 +1,18 @@
 package com.example.kickoffbackend.team.repository.custom.impl;
 
+import com.example.kickoffbackend.team.domain.QTeam;
+import com.example.kickoffbackend.team.domain.QTeamImage;
+import com.example.kickoffbackend.team.domain.QTeamMember;
+import com.example.kickoffbackend.team.domain.Team;
 import com.example.kickoffbackend.team.repository.custom.TeamCustom;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+
+import java.util.Optional;
+
+import static com.example.kickoffbackend.team.domain.QTeam.*;
+import static com.example.kickoffbackend.team.domain.QTeamImage.teamImage;
+import static com.example.kickoffbackend.team.domain.QTeamMember.teamMember;
 
 public class TeamCustomImpl implements TeamCustom {
 
@@ -13,4 +23,14 @@ public class TeamCustomImpl implements TeamCustom {
     }
 
 
+    @Override
+    public Optional<Team> findByName(String teamName) {
+        Team result = queryFactory
+                .selectFrom(team)
+                .leftJoin(team.teamImages, teamImage).fetchJoin()
+                .where(team.teamName.eq(teamName))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
 }
