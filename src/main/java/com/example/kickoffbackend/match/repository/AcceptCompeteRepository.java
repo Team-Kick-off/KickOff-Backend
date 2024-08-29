a745b3269a591fda125a6b300f83980429b6a32c
jaeheeeKim <jh960902@naver.com>
2024-08-29 19:10:16 +0900

    [FIX] 에러 수정

--- src/main/java/com/example/kickoffbackend/match/repository/AcceptCompeteRepository.java
+++ src/main/java/com/example/kickoffbackend/match/repository/AcceptCompeteRepository.java
@@ -1,5 +1,6 @@
-package com.example.kickoffbackend.match.domain;
+package com.example.kickoffbackend.match.repository;
 
+import com.example.kickoffbackend.match.domain.AcceptCompete;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AcceptCompeteRepository extends JpaRepository<AcceptCompete, Long> {
